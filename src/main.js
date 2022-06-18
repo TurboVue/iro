09bdedb4256c8f6663ab3ad6ccea5cc9e30c2096
guxukai <gxk@33.cn>
2022-06-19 07:24:23 +0800

    ...

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import "@src/styles/style.scss";
 
 import router from "@src/router";
 import i18n from "@src/i18n";
+import OneSignalVuePlugin from "@onesignal/onesignal-vue3";
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -16,7 +17,11 @@ const pinia = createPinia();
 app
   .use(pinia)
   .use(i18n)
-  .use(router);
+  .use(router)
+    .use(OneSignalVuePlugin, {
+        appId: '0e2742c8-4370-4441-88f7-8a172a5baa5a',
+        allowLocalhostAsSecureOrigin: true
+    })
 
 if (isRunningOnPWA()) window.resizeTo(400, 800);
 
