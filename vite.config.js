9f9a1934e58e33476ca99a8e20ad17644e6d969d
guxukai <gxk@33.cn>
2022-06-18 11:35:37 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import VueI18n from "@intlify/vite-plugin-vue-i18n";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       "@src": resolve(__dirname, "src"),
